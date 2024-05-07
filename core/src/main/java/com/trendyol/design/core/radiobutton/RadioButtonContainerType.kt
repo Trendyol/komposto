bec87262bd22cd184f42253b112b41f85984f6f4
goksu.turker <goksu.turker@trendyol.com>
2024-05-07 10:32:18 +0300

    ANDDEV-7343 | Add code documentation

--- core/src/main/java/com/trendyol/design/core/radiobutton/RadioButtonContainerType.kt
+++ core/src/main/java/com/trendyol/design/core/radiobutton/RadioButtonContainerType.kt
@@ -4,8 +4,14 @@ import androidx.compose.material.RadioButtonColors
 import androidx.compose.runtime.Composable
 import com.trendyol.theme.TrendyolDesign
 
+/**
+ * Interface defining what to display after radio dot while using [TrendyolRadioButton].
+ */
 interface RadioButtonContainerType {
 
+    /**
+     * Defines the colors used for rendering the RadioButton.
+     */
     val colors: RadioButtonColors
         @Composable
         get() = TrendyolDefaultRadioButtonColors(
@@ -15,12 +21,21 @@ interface RadioButtonContainerType {
             disabledUnselectedColor = TrendyolDesign.colors.colorBorder,
         )
 
+    /**
+     * Represents a RadioButton container with no additional properties. Just showing radio dot.
+     */
     interface None : RadioButtonContainerType
 
+    /**
+     * Represents a RadioButton container with a text.
+     */
     interface Text : RadioButtonContainerType {
         val text: String
     }
 
+    /**
+     * Represents a RadioButton container with custom composable content.
+     */
     interface Content : RadioButtonContainerType {
         val content: @Composable () -> Unit
     }
