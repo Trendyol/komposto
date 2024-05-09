ddc7b5f0697ed90e3ac3cd93bb7aa9c14ec8d0e6
irem.dogan <irem.dogan@trendyol.com>
2024-05-09 17:51:11 +0300

    ANDDEV-7339 | fix color issues

--- core/src/main/java/com/trendyol/design/core/inputfield/MultiLineOutlinedTextField.kt
+++ core/src/main/java/com/trendyol/design/core/inputfield/MultiLineOutlinedTextField.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
 import androidx.compose.material.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -17,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.trendyol.design.core.text.Text
 import com.trendyol.theme.TrendyolDesign
 
 /**
