1864d03c54832f652c5718b156471ad9fe35058f
irem.dogan <irem.dogan@trendyol.com>
2024-05-28 14:28:29 +0300

    ANDDEV-7339 | revert MainActivity

--- app/src/main/java/com/trendyol/design/MainActivity.kt
+++ app/src/main/java/com/trendyol/design/MainActivity.kt
@@ -3,20 +3,15 @@ package com.trendyol.design
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.trendyol.design.core.icon.Icons
+import com.trendyol.design.core.icon.StateLayoutIconSize
 import com.trendyol.design.core.icon.icons.outline.Back
-import com.trendyol.design.core.inputfield.Dropdown
-import com.trendyol.design.core.inputfield.MultiLineOutlineTextField
-import com.trendyol.design.core.inputfield.SingleLineOutlinedTextField
-import com.trendyol.design.core.inputfield.TrendyolDropdownStyle
-import com.trendyol.design.core.inputfield.TrendyolOutlinedTextFieldStyle
 import com.trendyol.design.core.statelayout.State
 import com.trendyol.design.core.statelayout.StateComposeLayout
+import com.trendyol.design.core.statelayout.TrendyolWarningInfoStateLayoutStyle
 import com.trendyol.design.core.toolbar.Toolbar
 import com.trendyol.design.core.toolbar.TrendyolToolbarStyle
 import com.trendyol.design.ui.theme.TrendyolTheme
@@ -46,43 +41,12 @@ class MainActivity : ComponentActivity() {
                 ) { paddingValues ->
                     StateComposeLayout(
                         modifier = Modifier.padding(paddingValues),
-                        state = State.ContentWithLoading(null),
-                        contentStateLayout = {
-                            Column(
-                                modifier = Modifier.padding(8.dp),
-
-                            ) {
-                                SingleLineOutlinedTextField(
-                                    modifier = Modifier.padding(8.dp),
-                                    style = TrendyolOutlinedTextFieldStyle.FloatingLabelOutlined,
-                                    label = "Label",
-                                    value = "",
-                                    errorLabel = "Error Label",
-                                    isError = false,
-                                    enabled = true
-                                ) {}
-
-                                MultiLineOutlineTextField(
-                                    modifier = Modifier.padding(8.dp),
-                                    style = TrendyolOutlinedTextFieldStyle.Outlined,
-                                    value = "",
-                                    maxChar = 20000,
-                                    label = "Label",
-                                    errorLabel = "Error Label",
-                                    isError = true,
-                                ) {}
-
-                                Dropdown(
-                                    modifier = Modifier.padding(8.dp),
-                                    style = TrendyolDropdownStyle.Filled,
-                                    value = "",
-                                    label = "Label",
-                                    errorLabel = "errorLabel",
-                                    isError = true,
-                                ) {}
-                            }
-
-                        }
+                        state = State.WarningInfo(
+                            warningInfoStateLayoutStyle = TrendyolWarningInfoStateLayoutStyle.SingleButtonNoDescription(
+                                iconSize = StateLayoutIconSize.WarningInfoStateLayoutMediumIcon,
+                                primaryButtonClickListener = {}
+                            )
+                        )
                     )
                 }
             }
