5ad20acab4925b97c41d9bdb62a772727890a3bc
irem.dogan <irem.dogan@trendyol.com>
2024-07-11 15:29:10 +0300

    ANDDEV-7528 | remove unused imports

--- core/src/main/java/com/trendyol/design/core/search/SearchBar.kt
+++ core/src/main/java/com/trendyol/design/core/search/SearchBar.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TextFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
