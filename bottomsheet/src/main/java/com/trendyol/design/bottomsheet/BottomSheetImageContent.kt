ab927c70a942a8136ed7318a413c2667d70463f7
goksu.turker <goksu.turker@trendyol.com>
2024-07-23 13:38:15 +0300

    MCHECKOUT-2507 | Replace interfaces with Composable APIs

--- bottomsheet/src/main/java/com/trendyol/design/bottomsheet/BottomSheetImageContent.kt
+++ bottomsheet/src/main/java/com/trendyol/design/bottomsheet/BottomSheetImageContent.kt
@@ -1,28 +1,36 @@
 package com.trendyol.design.bottomsheet
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.trendyol.design.bottomsheet.header.BottomSheetHeader
 
-interface BottomSheetImageContent : BottomSheetContent {
-
-    @Composable
-    fun imageUrl(): String
-
-    @Composable
-    override fun PageContent() {
-        Column {
-            Header()
-            AsyncImage(
-                model = imageUrl(),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues = outerPadding()),
-            )
-        }
+@Composable
+fun BottomSheetImageContent(
+    title: String,
+    onCloseIconClick: () -> Unit,
+    model: Any?,
+    modifier: Modifier = Modifier,
+    header: @Composable () -> Unit = {
+        BottomSheetHeader(title = title, onCloseIconClick = onCloseIconClick)
+    },
+    outerPadding: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        header()
+        AsyncImage(
+            model = model,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = outerPadding),
+        )
     }
 }
--- bottomsheet/src/main/java/com/trendyol/design/bottomsheet/BottomSheetSliderContent.kt
+++ bottomsheet/src/main/java/com/trendyol/design/bottomsheet/BottomSheetSliderContent.kt
@@ -13,41 +13,59 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
+import com.trendyol.design.bottomsheet.header.BottomSheetHeader
 
-interface BottomSheetSliderContent<Item> : BottomSheetContent, ListContract<Item> {
-
-    @Composable
-    override fun DividerContent() {
-        Spacer(modifier = Modifier.width(12.dp))
+@Composable
+fun <Item> BottomSheetSliderContent(
+    title: String,
+    onCloseIconClick: () -> Unit,
+    itemList: List<Item>,
+    itemContent: @Composable (index: Int, item: Item) -> Unit,
+    modifier: Modifier = Modifier,
+    header: @Composable () -> Unit = {
+        BottomSheetHeader(title = title, onCloseIconClick = onCloseIconClick)
+    },
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        header()
+        SliderContent(
+            itemList = itemList,
+            itemContent = itemContent,
+        )
     }
+}
 
-    @Composable
-    override fun PageContent() {
-        Column {
-            Header()
-            val itemList = itemList()
-            val outerPadding = outerPadding()
-            LazyRow(
-                modifier = Modifier.padding(
-                    paddingValues = PaddingValues(
-                        top = outerPadding.calculateTopPadding(),
-                        bottom = outerPadding.calculateBottomPadding(),
-                    )
-                ),
-            ) {
-                item {
-                    Spacer(modifier = Modifier.width(outerPadding.calculateStartPadding(LocalLayoutDirection.current)))
-                }
-                itemsIndexed(items = itemList) { index, item ->
-                    ItemContent(item)
-                    if (index != itemList.lastIndex) {
-                        DividerContent()
-                    }
-                }
-                item {
-                    Spacer(modifier = Modifier.width(outerPadding.calculateEndPadding(LocalLayoutDirection.current)))
-                }
+@Composable
+fun <Item> SliderContent(
+    itemList: List<Item>,
+    itemContent: @Composable (index: Int, item: Item) -> Unit,
+    modifier: Modifier = Modifier,
+    outerPadding: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+    dividerContent: @Composable (index: Int, item: Item) -> Unit = { _, _ ->
+        Spacer(modifier = Modifier.width(12.dp))
+    },
+) {
+    LazyRow(
+        modifier = modifier.padding(
+            paddingValues = PaddingValues(
+                top = outerPadding.calculateTopPadding(),
+                bottom = outerPadding.calculateBottomPadding(),
+            )
+        ),
+    ) {
+        item {
+            Spacer(modifier = Modifier.width(outerPadding.calculateStartPadding(LocalLayoutDirection.current)))
+        }
+        itemsIndexed(items = itemList) { index, item ->
+            itemContent(index, item)
+            if (index != itemList.lastIndex) {
+                dividerContent(index, item)
             }
+        }
+        item {
+            Spacer(modifier = Modifier.width(outerPadding.calculateEndPadding(LocalLayoutDirection.current)))
         }
     }
 }
