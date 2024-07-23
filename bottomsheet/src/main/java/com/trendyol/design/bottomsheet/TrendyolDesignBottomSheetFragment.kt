eb23772044cb33558e89c1c188e57eef11340a72
goksu.turker <goksu.turker@trendyol.com>
2024-07-23 13:44:50 +0300

    MCHECKOUT-2507 | Remove ListContract.kt as we are not proceeding with interface implementation  - Change TrendyolDesignBottomSheetFragment impl

--- bottomsheet/src/main/java/com/trendyol/design/bottomsheet/TrendyolDesignBottomSheetFragment.kt
+++ bottomsheet/src/main/java/com/trendyol/design/bottomsheet/TrendyolDesignBottomSheetFragment.kt
@@ -1,18 +1,27 @@
 package com.trendyol.design.bottomsheet
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.trendyol.design.bottomsheet.header.BottomSheetHeader
 
-abstract class TrendyolDesignBottomSheetFragment : BottomSheetDialogFragment(), BottomSheetContent {
+abstract class TrendyolDesignBottomSheetFragment : BottomSheetDialogFragment() {
+
+    @Composable
+    protected abstract fun PageContent()
 
     override fun getTheme(): Int = R.style.BaseBottomSheetDialog
 
-    @Composable
-    override fun Header() {
-        BottomSheetHeader(
-            title = title(),
-            onCloseIconClick = { dismiss() },
-        )
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(layoutInflater.context).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner))
+        setContent { PageContent() }
     }
 }
