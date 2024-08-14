f5d346b51b125f2d128c9239dcad0157af77782c
Perihan Mirkelam <perihan.mirkelam@dolap.com>
2024-08-14 16:14:54 +0300

    ANDDEV-7834 Create badge icons

---
+++ core/src/main/java/com/trendyol/design/core/icon/icons/fill/Percentage.kt
@@ -0,0 +1,105 @@
+package com.trendyol.design.core.icon.icons.fill
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.trendyol.design.core.icon.Icon
+import com.trendyol.design.core.icon.Icons
+import com.trendyol.design.core.icon.TrendyolIconSize
+
+@Preview
+@Composable
+private fun VectorPreview() {
+    Icon(imageVector = Icons.Fill.Percentage, size = TrendyolIconSize.Large)
+}
+
+val Icons.Fill.Percentage: ImageVector by lazy {
+    ImageVector.Builder(
+        name = "Percentage",
+        defaultWidth = 14.dp,
+        defaultHeight = 14.dp,
+        viewportWidth = 14f,
+        viewportHeight = 14f
+    ).apply {
+        path(
+            fill = SolidColor(Color(0xFFF27A1A)),
+            pathFillType = PathFillType.EvenOdd
+        ) {
+            moveTo(2.42f, 10.193f)
+            lineTo(3.077f, 10.583f)
+            curveTo(3.217f, 10.666f, 3.333f, 10.783f, 3.416f, 10.922f)
+            lineTo(3.807f, 11.58f)
+            curveTo(3.98f, 11.871f, 4.292f, 12.051f, 4.63f, 12.055f)
+            lineTo(5.395f, 12.064f)
+            curveTo(5.557f, 12.066f, 5.716f, 12.109f, 5.858f, 12.189f)
+            lineTo(6.525f, 12.563f)
+            curveTo(6.82f, 12.728f, 7.18f, 12.728f, 7.475f, 12.563f)
+            lineTo(8.142f, 12.189f)
+            curveTo(8.284f, 12.109f, 8.443f, 12.066f, 8.605f, 12.064f)
+            lineTo(9.37f, 12.055f)
+            curveTo(9.708f, 12.051f, 10.02f, 11.871f, 10.193f, 11.58f)
+            lineTo(10.584f, 10.922f)
+            curveTo(10.667f, 10.783f, 10.783f, 10.666f, 10.923f, 10.583f)
+            lineTo(11.58f, 10.193f)
+            curveTo(11.871f, 10.02f, 12.051f, 9.708f, 12.056f, 9.369f)
+            lineTo(12.065f, 8.605f)
+            curveTo(12.067f, 8.443f, 12.11f, 8.283f, 12.189f, 8.142f)
+            lineTo(12.563f, 7.475f)
+            curveTo(12.729f, 7.18f, 12.729f, 6.819f, 12.563f, 6.524f)
+            lineTo(12.189f, 5.857f)
+            curveTo(12.11f, 5.716f, 12.067f, 5.557f, 12.065f, 5.394f)
+            lineTo(12.056f, 4.63f)
+            curveTo(12.051f, 4.291f, 11.871f, 3.979f, 11.58f, 3.806f)
+            lineTo(10.923f, 3.416f)
+            curveTo(10.783f, 3.333f, 10.667f, 3.216f, 10.584f, 3.077f)
+            lineTo(10.193f, 2.42f)
+            curveTo(10.02f, 2.128f, 9.708f, 1.948f, 9.37f, 1.944f)
+            lineTo(8.605f, 1.935f)
+            curveTo(8.443f, 1.933f, 8.284f, 1.89f, 8.142f, 1.811f)
+            lineTo(7.475f, 1.437f)
+            curveTo(7.18f, 1.271f, 6.82f, 1.271f, 6.525f, 1.437f)
+            lineTo(5.858f, 1.811f)
+            curveTo(5.716f, 1.89f, 5.557f, 1.933f, 5.395f, 1.935f)
+            lineTo(4.63f, 1.944f)
+            curveTo(4.292f, 1.948f, 3.98f, 2.128f, 3.807f, 2.42f)
+            lineTo(3.416f, 3.077f)
+            curveTo(3.333f, 3.216f, 3.217f, 3.333f, 3.077f, 3.416f)
+            lineTo(2.42f, 3.806f)
+            curveTo(2.129f, 3.979f, 1.949f, 4.291f, 1.944f, 4.63f)
+            lineTo(1.935f, 5.394f)
+            curveTo(1.933f, 5.557f, 1.89f, 5.716f, 1.811f, 5.857f)
+            lineTo(1.437f, 6.524f)
+            curveTo(1.271f, 6.819f, 1.271f, 7.18f, 1.437f, 7.475f)
+            lineTo(1.811f, 8.142f)
+            curveTo(1.89f, 8.283f, 1.933f, 8.443f, 1.935f, 8.605f)
+            lineTo(1.944f, 9.369f)
+            curveTo(1.949f, 9.708f, 2.129f, 10.02f, 2.42f, 10.193f)
+            close()
+            moveTo(5.124f, 9.233f)
+            curveTo(4.919f, 9.06f, 4.892f, 8.754f, 5.065f, 8.548f)
+            lineTo(8.187f, 4.827f)
+            curveTo(8.36f, 4.621f, 8.666f, 4.594f, 8.872f, 4.767f)
+            curveTo(9.077f, 4.939f, 9.104f, 5.246f, 8.932f, 5.451f)
+            lineTo(5.809f, 9.173f)
+            curveTo(5.636f, 9.378f, 5.33f, 9.405f, 5.124f, 9.233f)
+            close()
+            moveTo(5.97f, 5.969f)
+            curveTo(6.35f, 5.59f, 6.35f, 4.975f, 5.97f, 4.595f)
+            curveTo(5.591f, 4.216f, 4.976f, 4.216f, 4.596f, 4.595f)
+            curveTo(4.217f, 4.975f, 4.217f, 5.59f, 4.596f, 5.969f)
+            curveTo(4.976f, 6.349f, 5.591f, 6.349f, 5.97f, 5.969f)
+            close()
+            moveTo(9.403f, 8.031f)
+            curveTo(9.782f, 8.41f, 9.782f, 9.025f, 9.403f, 9.405f)
+            curveTo(9.024f, 9.784f, 8.408f, 9.784f, 8.029f, 9.405f)
+            curveTo(7.649f, 9.025f, 7.649f, 8.41f, 8.029f, 8.031f)
+            curveTo(8.408f, 7.651f, 9.024f, 7.651f, 9.403f, 8.031f)
+            close()
+        }
+    }.build()
+}
