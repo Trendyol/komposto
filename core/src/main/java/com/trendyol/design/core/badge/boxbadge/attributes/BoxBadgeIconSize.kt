9d33ec665bf50b5309573708d4566468cfaaf52a
Perihan Mirkelam <perihan.mirkelam@dolap.com>
2024-08-14 17:15:53 +0300

    ANDDEV-7834 Create attributes

---
+++ core/src/main/java/com/trendyol/design/core/badge/boxbadge/attributes/BoxBadgeIconSize.kt
@@ -0,0 +1,9 @@
+package com.trendyol.design.core.badge.boxbadge.attributes
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.trendyol.design.core.icon.IconSize
+
+data object BoxBadgeIconSize : IconSize {
+    override val dp: Dp = 14.dp
+}
