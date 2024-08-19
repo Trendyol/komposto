6b141b329f83f365a4d400c72228531f8c4d3d90
Perihan Mirkelam <perihan.mirkelam@dolap.com>
2024-08-19 10:27:33 +0300

    ANDDEV-7834 Separate predefined and dynamic badges

--- core/src/main/java/com/trendyol/design/core/badge/boxbadge/BoxBadgeType.kt
+++ core/src/main/java/com/trendyol/design/core/badge/boxbadge/BoxBadgeType.kt
@@ -15,94 +15,99 @@ import com.trendyol.design.core.icon.icons.fill.PlayGradient
 
 sealed class BoxBadgeType(val badge: BoxBadgeModel) {
 
-    data class Custom(
+    data class Dynamic(
         val title: String,
         val icon: ImageVector,
         val backgroundColor: Color,
+        val iconTint: Color = Color.Unspecified
     ) : BoxBadgeType(
         BoxBadgeModel(
             title = title,
             backgroundColor = backgroundColor,
             icon = icon,
+            iconTint = iconTint,
         )
     )
 
-    data object Coupon : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Kupon Fırsatı",
-            backgroundColor = Color(color = 0xFFFFF0F5),
-            icon = Icons.Fill.Coupon,
-            iconTint = Color(color = 0xFFFF4988)
-        )
-    )
+    object Defaults {
 
-    data object FreeDelivery : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Kargo Bedava",
-            backgroundColor = Color(color = 0xFFF7F7F7),
-            icon = Icons.Fill.Cargo,
-            iconTint = Color(color = 0xFF666666)
+        data object Coupon : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Kupon Fırsatı",
+                backgroundColor = Color(color = 0xFFFFF0F5),
+                icon = Icons.Fill.Coupon,
+                iconTint = Color(color = 0xFFFF4988)
+            )
         )
-    )
 
-    data object FastDelivery : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Hızlı Teslimat",
-            backgroundColor = Color(color = 0xFFEBFAF2),
-            icon = Icons.Fill.Delivery,
-            iconTint = Color(color = 0xFF0BC15C)
+        data object FreeDelivery : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Kargo Bedava",
+                backgroundColor = Color(color = 0xFFF7F7F7),
+                icon = Icons.Fill.Cargo,
+                iconTint = Color(color = 0xFF666666)
+            )
         )
-    )
 
-    data object BuyMorePayLess : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Çok Al Az Öde",
-            backgroundColor = Color(color = 0xFFFEF4ED),
-            icon = Icons.Fill.Campaign,
-            iconTint = Color(color = 0xFFF27A1A)
+        data object FastDelivery : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Hızlı Teslimat",
+                backgroundColor = Color(color = 0xFFEBFAF2),
+                icon = Icons.Fill.Delivery,
+                iconTint = Color(color = 0xFF0BC15C)
+            )
         )
-    )
 
-    data object BuyTogether : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Birlikte Al Kazan",
-            backgroundColor = Color(color = 0xFFFEF4ED),
-            icon = Icons.Fill.Percentage,
-            iconTint = Color(color = 0xFFF27A1A)
+        data object BuyMorePayLess : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Çok Al Az Öde",
+                backgroundColor = Color(color = 0xFFFEF4ED),
+                icon = Icons.Fill.Campaign,
+                iconTint = Color(color = 0xFFF27A1A)
+            )
         )
-    )
 
-    data object Video : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Videolu Ürün",
-            backgroundColor = Color(color = 0xFFF6EFFF),
-            icon = Icons.Fill.PlayGradient,
+        data object BuyTogether : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Birlikte Al Kazan",
+                backgroundColor = Color(color = 0xFFFEF4ED),
+                icon = Icons.Fill.Percentage,
+                iconTint = Color(color = 0xFFF27A1A)
+            )
         )
-    )
 
-    data object TodayDelivery : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Bugün Kargoda",
-            backgroundColor = Color(color = 0xFFCFFFE4),
-            icon = Icons.Fill.Delivery,
-            iconTint = Color(color = 0xFF159F52),
+        data object Video : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Videolu Ürün",
+                backgroundColor = Color(color = 0xFFF6EFFF),
+                icon = Icons.Fill.PlayGradient,
+            )
         )
-    )
 
-    data object Credit : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Krediye Uygun",
-            backgroundColor = Color(color = 0xFFFFF9E5),
-            icon = Icons.Fill.Credit,
-            iconTint = Color(color = 0xFFFFC000),
+        data object TodayDelivery : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Bugün Kargoda",
+                backgroundColor = Color(color = 0xFFCFFFE4),
+                icon = Icons.Fill.Delivery,
+                iconTint = Color(color = 0xFF159F52),
+            )
         )
-    )
 
-    data object InfluencerChoice : BoxBadgeType(
-        BoxBadgeModel(
-            title = "Fenomen Seçimi",
-            backgroundColor = Color(color = 0xFFFEF5ED),
-            icon = Icons.Fill.FenomenGradient,
+        data object Credit : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Krediye Uygun",
+                backgroundColor = Color(color = 0xFFFFF9E5),
+                icon = Icons.Fill.Credit,
+                iconTint = Color(color = 0xFFFFC000),
+            )
         )
-    )
+
+        data object InfluencerChoice : BoxBadgeType(
+            BoxBadgeModel(
+                title = "Fenomen Seçimi",
+                backgroundColor = Color(color = 0xFFFEF5ED),
+                icon = Icons.Fill.FenomenGradient,
+            )
+        )
+    }
 }
