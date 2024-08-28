393da0bb0e7429c6e0e27e117a64d9c22e51d7fb
Perihan Mirkelam <perihan.mirkelam@dolap.com>
2024-08-28 14:49:08 +0300

    ANDDEV-7834 Remove display mode

--- core/src/main/java/com/trendyol/design/core/badge/boxbadge/BoxBadgeList.kt
+++ core/src/main/java/com/trendyol/design/core/badge/boxbadge/BoxBadgeList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -14,7 +13,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.trendyol.design.core.badge.boxbadge.model.BoxBadgeListAttributes
 import com.trendyol.design.core.badge.boxbadge.model.BoxBadgeListContent
-import com.trendyol.design.core.badge.boxbadge.model.BoxBadgeListDisplayMode
 import com.trendyol.design.core.icon.Icons
 import com.trendyol.design.core.icon.icons.fill.Help
 import com.trendyol.design.core.preview.PreviewTheme
@@ -23,50 +21,37 @@ import com.trendyol.theme.TrendyolDesign
 /**
  * Represents a list of badges displayed in either a spread or fixed mode.
  *
- * @param list List of badges to be displayed.
+ * @param content List of badges to be displayed.
  * @param modifier Modifier to be applied to the list.
  * @param attributes Configuration options for the badge list,
- * including display mode, spacing, and item attributes.
+ * including badge count, spacing, and item attributes.
  */
 @Composable
 fun BoxBadgeList(
     content: BoxBadgeListContent,
     modifier: Modifier = Modifier,
     attributes: BoxBadgeListAttributes = BoxBadgeListAttributes(),
-) {
-    val visibleBadgeCount = attributes.visibleBadgeCount
-    val itemsToShow = remember(content) { content.list.take(visibleBadgeCount) }
-    val emptySlots = visibleBadgeCount - itemsToShow.size
-    val itemAttributes = attributes.itemAttributes
+) = with(attributes) {
+
+    val itemsToShow = remember(content) { content.list.take(maxBadgeCount) }
+    val emptySlots = maxBadgeCount - itemsToShow.size
+    val defaultWidth = (itemAttributes.boxWidth * maxBadgeCount) +
+            (maxBadgeCount.dec() * attributes.space)
 
     Row(
-        modifier = when (attributes.displayMode) {
-            BoxBadgeListDisplayMode.SPREAD -> modifier.fillMaxWidth()
-            BoxBadgeListDisplayMode.FIXED -> modifier
-        },
-        horizontalArrangement = Arrangement.spacedBy(attributes.space),
+        modifier = modifier.then(Modifier.width(defaultWidth.dp)),
+        horizontalArrangement = Arrangement.spacedBy(attributes.space.dp),
     ) {
-        val itemModifier = when (attributes.displayMode) {
-
-            BoxBadgeListDisplayMode.SPREAD -> {
-                val ratio = itemAttributes.boxWidth.toFloat() / itemAttributes.boxHeight.toFloat()
-                Modifier
-                    .weight(1f)
-                    .aspectRatio(ratio)
-            }
-
-            BoxBadgeListDisplayMode.FIXED -> {
-                Modifier
-                    .width(itemAttributes.boxWidth.dp)
-                    .height(itemAttributes.boxHeight.dp)
-            }
-        }
+        val ratio = itemAttributes.boxWidth.toFloat() / itemAttributes.boxHeight.toFloat()
+        val itemModifier = Modifier
+            .weight(1f)
+            .aspectRatio(ratio)
 
         itemsToShow.forEach { item ->
             BoxBadge(
                 boxBadgeType = item,
                 modifier = itemModifier,
-                attributes = attributes.itemAttributes,
+                attributes = itemAttributes,
             )
         }
 
@@ -78,7 +63,7 @@ fun BoxBadgeList(
 
 @Preview(showBackground = false)
 @Composable
-private fun Fixed4BadgesListPreview() = PreviewTheme {
+private fun Default4BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Dynamic(
             title = "Dynamic Badge",
@@ -91,60 +76,48 @@ private fun Fixed4BadgesListPreview() = PreviewTheme {
     )
     BoxBadgeList(
         content = BoxBadgeListContent(list),
-        attributes = BoxBadgeListAttributes().copy(
-            displayMode = BoxBadgeListDisplayMode.FIXED
-        ),
     )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Fixed3BadgesListPreview() = PreviewTheme {
+private fun Default3BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.BuyMorePayLess,
         BoxBadgeType.Defaults.BuyTogether,
         BoxBadgeType.Defaults.Video,
     )
     BoxBadgeList(
-        content = BoxBadgeListContent(list),
-        attributes = BoxBadgeListAttributes().copy(
-            displayMode = BoxBadgeListDisplayMode.FIXED
-        ),
+        content = BoxBadgeListContent(list)
     )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Fixed2BadgesListPreview() = PreviewTheme {
+private fun Default2BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.TodayDelivery,
         BoxBadgeType.Defaults.Credit,
     )
     BoxBadgeList(
         content = BoxBadgeListContent(list),
-        attributes = BoxBadgeListAttributes().copy(
-            displayMode = BoxBadgeListDisplayMode.FIXED
-        ),
     )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Fixed1BadgeListPreview() = PreviewTheme {
+private fun Default1BadgeListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.InfluencerChoice,
     )
     BoxBadgeList(
         content = BoxBadgeListContent(list),
-        attributes = BoxBadgeListAttributes().copy(
-            displayMode = BoxBadgeListDisplayMode.FIXED
-        ),
     )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Spread4BadgesListPreview() = PreviewTheme {
+private fun Scaled4BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Dynamic(
             title = "Dynamic Badge",
@@ -155,63 +128,47 @@ private fun Spread4BadgesListPreview() = PreviewTheme {
         BoxBadgeType.Defaults.FreeDelivery,
         BoxBadgeType.Defaults.FastDelivery,
     )
-    Row(modifier = Modifier.width(200.dp)) {
-        BoxBadgeList(
-            content = BoxBadgeListContent(list),
-            attributes = BoxBadgeListAttributes().copy(
-                displayMode = BoxBadgeListDisplayMode.SPREAD
-            ),
-        )
-    }
+    BoxBadgeList(
+        content = BoxBadgeListContent(list),
+        modifier = Modifier.fillMaxWidth(),
+    )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Spread3BadgesListPreview() = PreviewTheme {
+private fun Scaled3BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.BuyMorePayLess,
         BoxBadgeType.Defaults.BuyTogether,
         BoxBadgeType.Defaults.Video,
     )
-    Row(modifier = Modifier.width(200.dp)) {
-        BoxBadgeList(
-            content = BoxBadgeListContent(list),
-            attributes = BoxBadgeListAttributes().copy(
-                displayMode = BoxBadgeListDisplayMode.SPREAD
-            ),
-        )
-    }
+    BoxBadgeList(
+        content = BoxBadgeListContent(list),
+        modifier = Modifier.fillMaxWidth(),
+    )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Spread2BadgesListPreview() = PreviewTheme {
+private fun Scaled2BadgesListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.TodayDelivery,
         BoxBadgeType.Defaults.Credit,
     )
-    Row(modifier = Modifier.width(200.dp)) {
-        BoxBadgeList(
-            content = BoxBadgeListContent(list),
-            attributes = BoxBadgeListAttributes().copy(
-                displayMode = BoxBadgeListDisplayMode.SPREAD
-            ),
-        )
-    }
+    BoxBadgeList(
+        content = BoxBadgeListContent(list),
+        modifier = Modifier.fillMaxWidth(),
+    )
 }
 
 @Preview(showBackground = false)
 @Composable
-private fun Spread1BadgeListPreview() = PreviewTheme {
+private fun Scaled1BadgeListPreview() = PreviewTheme {
     val list = listOf(
         BoxBadgeType.Defaults.InfluencerChoice,
     )
-    Row(modifier = Modifier.width(200.dp)) {
-        BoxBadgeList(
-            content = BoxBadgeListContent(list),
-            attributes = BoxBadgeListAttributes().copy(
-                displayMode = BoxBadgeListDisplayMode.SPREAD
-            ),
-        )
-    }
+    BoxBadgeList(
+        content = BoxBadgeListContent(list),
+        modifier = Modifier.fillMaxWidth(),
+    )
 }
--- core/src/main/java/com/trendyol/design/core/badge/boxbadge/model/BoxBadgeListDisplayMode.kt
+++
@@ -1,5 +0,0 @@
-package com.trendyol.design.core.badge.boxbadge.model
-
-enum class BoxBadgeListDisplayMode {
-    SPREAD, FIXED
-}
