405d5aabc4335dd04aaf1641bd02d192303d8820
yasemin.aksoy <yasemin.aksoy@trendyol.com>
2024-10-17 11:39:27 +0300

    remove data class and add all themes in style

--- core/src/main/java/com/trendyol/design/core/infobox/InfoBoxStyle.kt
+++ core/src/main/java/com/trendyol/design/core/infobox/InfoBoxStyle.kt
@@ -1,42 +1,150 @@
 package com.trendyol.design.core.infobox
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.trendyol.theme.TrendyolDesign
 
 interface InfoBoxStyle {
-    val infoBoxModel: InfoBoxModel
+    @get:Composable
+    val backgroundColor: Color
+
+    @get:Composable
+    val borderColor: Color
+
+    @get:Composable
+    val textColor: Color
+
+    @get:Composable
+    val iconTint: Color
 }
 
-sealed interface TrendyolInfoBoxStyle {
+sealed interface KPInfoBoxStyle {
+    data object Orange : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimaryVariant1
 
-    data class DefaultInfoBox(
-        val textModel: ContentTextModel,
-        val icon: IconModel,
-        val iconPosition: IconPosition = IconPosition.START,
-        val backgroundColor: Color = Color.Unspecified,
-    ) : InfoBoxStyle {
-        override val infoBoxModel: InfoBoxModel
-            get() = InfoBoxModel(
-                textModel = textModel,
-                icon = icon,
-                backgroundColor = backgroundColor,
-                iconPosition = iconPosition
-            )
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimaryVariant2
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimary
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimary
+
     }
 
-    data class DefaultInfoBoxWithBorder(
-        val textModel: ContentTextModel,
-        val icon: IconModel,
-        val iconPosition: IconPosition = IconPosition.START,
-        val backgroundColor: Color = Color.Unspecified,
-        val borderColor: Color = Color.Unspecified
-    ) : InfoBoxStyle {
-        override val infoBoxModel: InfoBoxModel
-            get() = InfoBoxModel(
-                textModel = textModel,
-                icon = icon,
-                backgroundColor = backgroundColor,
-                iconPosition = iconPosition,
-                borderColor = borderColor
-            )
+    data object White : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorSurface
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBorder
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimary
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimary
+    }
+
+    data object Gray : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorOnSurfaceVariant1.copy(alpha = 0.12f)
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBorder
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorOnSurfaceVariant3
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPrimary
+    }
+
+    data object Red : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorWarningVariant1
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorWarningVariant2
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorWarning
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorWarning
+
+    }
+
+    data object Blue : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBlueVariant1
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBlueVariant2
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBlue
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorBlue
+
+    }
+
+    data object Green : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorGreenVariant1
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorGreenVariant2
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorGreen
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorGreen
+    }
+
+    data object Pink : InfoBoxStyle {
+        override val backgroundColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPinkVariant1
+
+        override val borderColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPinkVariant2
+
+        override val textColor: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPink
+
+        override val iconTint: Color
+            @Composable
+            get() = TrendyolDesign.colors.colorPink
     }
 }
