ebc43add8d96e336feb2dfbbf6b7644cb4e8cabc
yasemin.aksoy <yasemin.aksoy@trendyol.com>
2024-10-17 13:34:28 +0300

    add docs

--- core/src/main/java/com/trendyol/design/core/infobox/InfoBoxStyle.kt
+++ core/src/main/java/com/trendyol/design/core/infobox/InfoBoxStyle.kt
@@ -4,6 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.trendyol.theme.TrendyolDesign
 
+/**
+ * `InfoBoxStyle` is an interface that defines the styling properties for an information box.
+ * It provides customizable colors for the background, border, text, and icon.
+ *
+ * Each property is annotated with `@Composable`, allowing it to be used within composable functions
+ * and providing the ability to dynamically change styles based on the composition context.
+ *
+ * Properties:
+ *
+ * @property backgroundColor A [Color] that sets the background color of the info box.
+ * @property borderColor A [Color] that sets the border color of the info box.
+ * @property textColor A [Color] that sets the color of the text within the info box.
+ * @property iconTint A [Color] that sets the tint color for the icon displayed in the info box.
+ */
+
 interface InfoBoxStyle {
     @get:Composable
     public val backgroundColor: Color
