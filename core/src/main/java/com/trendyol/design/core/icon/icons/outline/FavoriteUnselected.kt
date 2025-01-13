011b02d051f08c65fd20cc11815dd86cd7c80c5f
goksu.turker <goksu.turker@trendyol.com>
2025-01-13 13:22:56 +0300

    ANDDEV-7875 | Migrate FavoriteUnselected

--- core/src/main/java/com/trendyol/design/core/icon/icons/outline/FavoriteUnselected.kt
+++ core/src/main/java/com/trendyol/design/core/icon/icons/outline/FavoriteUnselected.kt
@@ -9,17 +9,17 @@ import androidx.compose.ui.graphics.vector.group
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.trendyol.design.core.icon.Icon
-import com.trendyol.design.core.icon.Icons
-import com.trendyol.design.core.icon.TrendyolIconSize
+import com.trendyol.design.core.icon.KPIcon
+import com.trendyol.design.core.icon.KPIconSize
+import com.trendyol.design.core.icon.KPIcons
 
 @Preview
 @Composable
 private fun VectorPreview() {
-    Icon(Icons.Outline.FavoriteUnselected, TrendyolIconSize.XXXLarge)
+    KPIcon(KPIcons.Outline.FavoriteUnselected, KPIconSize.XXXLarge)
 }
 
-public val Icons.Outline.FavoriteUnselected: ImageVector
+public val KPIcons.Outline.FavoriteUnselected: ImageVector
     by lazy {
         ImageVector.Builder(
             name = "Favorite Unselected",
