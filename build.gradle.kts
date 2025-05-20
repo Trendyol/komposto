0b28904a23f86bf272193a17c52a0257378af663
goksu.turker <goksu.turker@trendyol.com>
2025-05-20 10:16:03 +0300

    ANDDEV-8665 | Create conventions  - Migrate rememberRipple to ripple in some composables  - Fix detekt issue in KPCountdownTimer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.android.library") version "8.3.2" apply false
-    id("io.gitlab.arturbosch.detekt") version("1.23.5")
-    id("com.google.devtools.ksp") version("1.9.24-1.0.20") apply false
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
-    id("org.jetbrains.dokka") version "1.9.20"
+    alias(libs.plugins.androidApplication).apply(false)
+    alias(libs.plugins.androidLibrary).apply(false)
+    alias(libs.plugins.composeCompiler).apply(false)
+    alias(libs.plugins.jetbrainsKotlinAndroid).apply(false)
+    alias(libs.plugins.detekt).apply(false)
+    alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.binaryCompatibilityValidator).apply(true)
+    alias(libs.plugins.dokkaDocumentationPlugin).apply(false)
 }
 
 apiValidation {
