a4541e95ce26e2983103a37554dac33bffae3840
burak <karadumanburak000@gmail.com>
2025-10-13 10:08:38 +0300

    GH-21 | Fixed mixed file naming for Back.kt and Cancel.kt

--- app/src/main/java/com/trendyol/design/ui/component/icons/outline/Cancel.kt
+++ app/src/main/java/com/trendyol/design/ui/component/icons/outline/Cancel.kt
@@ -5,54 +5,54 @@ import com.airbnb.android.showkase.annotation.ShowkaseComposable
 import com.trendyol.design.core.icon.KPIcon
 import com.trendyol.design.core.icon.KPIconSize
 import com.trendyol.design.core.icon.KPIcons
-import com.trendyol.design.core.icon.icons.outline.Back
+import com.trendyol.design.core.icon.icons.outline.Cancel
 import com.trendyol.design.ui.component.common.Component
 import com.trendyol.design.ui.component.common.Group
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "1.Outline.XXSmall")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "1.Outline.XXSmall")
 @Composable
-internal fun Back_Outline_XXSmall() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.XXSmall)
+internal fun Cancel_Outline_XXSmall() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.XXSmall)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "2.Outline.XSmall")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "2.Outline.XSmall")
 @Composable
-internal fun Back_Outline_XSmall() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.XSmall)
+internal fun Cancel_Outline_XSmall() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.XSmall)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "3.Outline.Small")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "3.Outline.Small")
 @Composable
-internal fun Back_Outline_Small() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.Small)
+internal fun Cancel_Outline_Small() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.Small)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "4.Outline.Medium")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "4.Outline.Medium")
 @Composable
-internal fun Back_Outline_Medium() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.Medium)
+internal fun Cancel_Outline_Medium() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.Medium)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "5.Outline.Large")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "5.Outline.Large")
 @Composable
-internal fun Back_Outline_Large() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.Large)
+internal fun Cancel_Outline_Large() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.Large)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "6.Outline.XLarge")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "6.Outline.XLarge")
 @Composable
-internal fun Back_Outline_XLarge() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.XLarge)
+internal fun Cancel_Outline_XLarge() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.XLarge)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "7.Outline.XXLarge")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "7.Outline.XXLarge")
 @Composable
-internal fun Back_Outline_XXLarge() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.XXLarge)
+internal fun Cancel_Outline_XXLarge() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.XXLarge)
 }
 
-@ShowkaseComposable(group = Group.ICON, name = Component.BACK_ICON, styleName = "8.Outline.XXXLarge")
+@ShowkaseComposable(group = Group.ICON, name = Component.CANCEL_ICON, styleName = "8.Outline.XXXLarge")
 @Composable
-internal fun Back_Outline_XXXLarge() {
-    KPIcon(imageVector = KPIcons.Outline.Back, size = KPIconSize.XXXLarge)
+internal fun Cancel_Outline_XXXLarge() {
+    KPIcon(imageVector = KPIcons.Outline.Cancel, size = KPIconSize.XXXLarge)
 }
